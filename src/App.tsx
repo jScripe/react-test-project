604c6963dbfcdd97eda2700483aabc6ac8b106ed
Sergei_Portsev <jiperscrip@gmail.com>
2020-06-20 12:33:18 +0400

    initial react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import cn from 'classnames';
+
+import styles from './App.module.scss';
+
+
+function App() {
+  return (
+    <div>
+      123
+    </div>
+  );
+}
+
+export default App;
